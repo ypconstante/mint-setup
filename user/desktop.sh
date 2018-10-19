fd83ae0d22ce1a44780e60c12410acf71762cbeb
Yuri Pereira <ypconstante@gmail.com>
2018-10-19 19:48:33 -0300

    replace gsettings with dconf

--- user/desktop.sh
+++ user/desktop.sh
@@ -8,14 +8,14 @@ echo "done: $STEP"
 
 STEP="modify taskbar"
 # position
-gsettings set org.cinnamon panels-enabled "['1:0:right']"
+dconf write /org/cinnamon/panels-enabled "['1:0:right']"
 # size
-gsettings set org.cinnamon panels-resizable "['1:true']"
-gsettings set org.cinnamon panels-height "['1:40']"
+dconf write /org/cinnamon/panels-resizable "['1:true']"
+dconf write /org/cinnamon/panels-height "['1:40']"
 # don't scale text
-gsettings set org.cinnamon panels-scale-text-icons "['1:false']"
+dconf write /org/cinnamon/panels-scale-text-icons "['1:false']"
 # taskbar elements
-gsettings set org.cinnamon enabled-applets "[
+dconf write /org/cinnamon/enabled-applets "[
  'panel1:left:0:menu@cinnamon.org',
  'panel1:left:3:window-list@cinnamon.org',
  'panel1:right:0:workspace-switcher@cinnamon.org',
@@ -29,5 +29,5 @@ gsettings set org.cinnamon enabled-applets "[
 echo "done: $STEP"
 
 STEP="modify background"
-gsettings set org.cinnamon.desktop.background picture-uri "'file:///usr/share/backgrounds/linuxmint-tara/whmii_toy_store.jpg'"
+dconf write /org/cinnamon/desktop/background/picture-uri "'file:///usr/share/backgrounds/linuxmint-tara/whmii_toy_store.jpg'"
 echo "done: $STEP"
