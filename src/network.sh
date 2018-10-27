d7e363cbccbb465c628a9b60b5e2ab91eab9db04
Yuri Pereira <ypconstante@gmail.com>
2018-10-27 17:58:25 -0300

    disable wifi power management

--- src/network.sh
+++ src/network.sh
@@ -5,3 +5,7 @@ set -x
 STEP="network kernel parameters"
 sudo cp ./files/network-sysctl.conf /etc/sysctl.d/98-network.conf
 echo "done: $STEP"
+
+STEP="disable wifi power management"
+sudo sed -i 's/wifi.powersave = 3/wifi.powersave = 2/' /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf
+echo "done: $STEP"
