85850b8ea869766c636a70c78b7fba50f3cbc038
Yuri Pereira <ypconstante@gmail.com>
2018-12-13 22:02:00 -0200

    Move from chrome to chromium

--- src/browser_chrome.sh
+++ src/browser_chrome.sh
@@ -3,8 +3,5 @@
 source "$(dirname "$0")/_base.sh"
 
 STEP="install chrome"
-curl -sS https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-echo "deb https://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google.list
-apt update -y -qq
-my_apt_install google-chrome-stable
+my_apt_install chromium-browser
 echo "done: $STEP"
