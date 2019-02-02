270acb2d7a86857e74f859c0c1822707685b74a0
Yuri Pereira <ypconstante@gmail.com>
2019-02-02 21:52:37 -0200

    Restore franz setup

---
+++ src/utility_franz.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source "$(dirname "$0")/_base.sh"
+
+my_step_begin "install franz"
+installer_path=/tmp/franz_installer.deb
+installer_url=$( \
+	curl -sS https://api.github.com/repos/meetfranz/franz/releases/latest \
+	| grep browser_download_url	 \
+	| grep .deb \
+	| sed -E 's/.*"([^"]+)".*/\1/' \
+)
+curl -L $installer_url -o $installer_path
+sudo dpkg --install $installer_path
+my_step_end
