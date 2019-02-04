93a589a5a824d32f31728304d888d6349055ee15
Yuri Pereira <ypconstante@gmail.com>
2019-02-04 21:57:02 -0200

    Fix asdf setup order

--- src/dev_asdf.sh
+++ src/dev_asdf.sh
@@ -13,10 +13,6 @@ my_step_begin "config asdf"
 my_link_file $ASSETS_DIR/dev_asdf--rc $ASDF_CONFIG_FILE
 my_step_end
 
-my_step_begin "import nodejs keys"
-source $ASDF_DATA_DIR/plugins/nodejs/bin/import-release-team-keyring
-my_step_end
-
 my_step_begin "install asdf plugins"
 # disable variable check since it's a external script
 set +o nounset
@@ -25,4 +21,8 @@ asdf plugin-add gradle
 asdf plugin-add java
 asdf plugin-add maven
 asdf plugin-add nodejs
+my_step_end
+
+my_step_begin "import nodejs keys"
+source $ASDF_DATA_DIR/plugins/nodejs/bin/import-release-team-keyring
 my_step_end
