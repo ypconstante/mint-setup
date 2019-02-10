88b883e8616256114e04c3c41c12136acc5c7b48
Yuri Pereira <ypconstante@gmail.com>
2019-02-10 19:45:50 -0200

    Add visual studio code install

---
+++ src/dev_visual-studio-code.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source "$(dirname "$0")/_base.sh"
+
+my_step_begin "install visual studio code"
+my_apt_add_key https://packages.microsoft.com/keys/microsoft.asc
+my_apt_add_repository vscode 'deb [arch=amd64] http://packages.microsoft.com/repos/vscode stable main'
+my_apt_install code
+my_step_end
+
+
+my_step_begin "configure visual studio code"
+my_link_file $ASSETS_DIR/dev_visual-studio-code--settings.json $XDG_CONFIG_HOME/Code/User/settings.json
+my_step_end
