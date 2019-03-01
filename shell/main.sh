97903cbeb44c6110f34b6526a1398fd06cf5c718
Yuri Pereira <ypconstante@gmail.com>
2019-02-28 21:47:49 -0300

    Sort main scripts

--- shell/main.sh
+++ shell/main.sh
@@ -5,9 +5,8 @@ source "$(dirname "$0")/_base.sh"
 ./prepare.sh
 ./terminal.sh
 ./browsers.sh
-./dev.sh
-./utilities.sh
 ./desktop.sh
+./dev.sh
 ./file-browser.sh
 ./file-system.sh
 ./git.sh
@@ -17,3 +16,4 @@ source "$(dirname "$0")/_base.sh"
 ./sound.sh
 ./system-monitor.sh
 ./update-manager.sh
+./utilities.sh
