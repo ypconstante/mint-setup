24dc22e1379cdee5733670e9cc593a8af71e15cf
Yuri Pereira <ypconstante@gmail.com>
2019-03-04 19:26:27 -0300

    Fix environment step name

--- shell/environment.sh
+++ shell/environment.sh
@@ -2,6 +2,6 @@
 
 source "$(dirname "$0")/_base.sh"
 
-my_step_begin "install zsh"
+my_step_begin "config environment variables"
 sed -E 's/^export ([^=]*)/\1 DEFAULT/g' "$ASSETS_DIR/base--env" > ~/.pam_environment
 my_step_end
