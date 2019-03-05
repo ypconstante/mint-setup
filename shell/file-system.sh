b1c87f296a3f0a03ca570edd610a558167ac6f22
Yuri Pereira <ypconstante@gmail.com>
2019-03-05 15:28:14 -0300

    Fix step not ended

--- shell/file-system.sh
+++ shell/file-system.sh
@@ -41,6 +41,7 @@ xdg-user-dirs-update --set DESKTOP "$HOME"
 rm -rf ~/Desktop
 xdg-user-dirs-update --set PUBLICSHARE "$(xdg-user-dir DOWNLOAD)"
 rm -rf ~/Public
+my_step_end
 
 my_step_begin "remove non used folders"
 rm -rf ~/.gconf
