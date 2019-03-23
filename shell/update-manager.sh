12e5eaf13b87a03cc228f9ad4a22a2ba47c2baae
Yuri Pereira <ypconstante@gmail.com>
2019-03-23 19:08:36 -0300

    Show package old version in update manager

--- shell/update-manager.sh
+++ shell/update-manager.sh
@@ -5,6 +5,7 @@ source "$(dirname "$0")/_base.sh"
 my_step_begin "modify update manager settings"
 dconf write /com/linuxmint/updates/hide-window-after-update true
 dconf write /com/linuxmint/updates/hide-systray true
+dconf write /com/linuxmint/updates/show-old-version-column true
 dconf write /com/linuxmint/updates/default-repo-is-ok true
 dconf write /com/linuxmint/updates/kernel-updates-are-safe false
 my_step_end
