a2f8a408d0ae42a7c6935b7675c3db3310074735
Yuri Pereira <ypconstante@gmail.com>
2019-10-03 00:29:36 -0300

    Mute on screen lock

--- assets/screen-lock--monitor.sh
+++ assets/screen-lock--monitor.sh
@@ -10,17 +10,37 @@ close_jetbrains_toolbox() {
     pkill jetbrains-toolb
 }
 
-run() {
+mute() {
+    amixer -q -D pulse sset Master mute
+}
+
+unmute() {
+    amixer -q -D pulse sset Master unmute
+}
+
+on_start() {
     disable_bluetooth
     close_jetbrains_toolbox
 }
 
-run
+on_lock() {
+    disable_bluetooth
+    close_jetbrains_toolbox
+    mute
+}
 
-dbus-monitor --session "type='signal',interface='org.cinnamon.ScreenSaver'" |
+on_unlock() {
+    unmute
+}
+
+on_start
+
+dbus-monitor --session "type='signal',interface='org.cinnamon.ScreenSaver',member='ActiveChanged'" |
     while read x; do
         case "$x" in
-            *"boolean true"*)
-                run;;
+            *"true"*)
+                on_lock;;
+            *"false"*)
+                on_unlock;;
         esac
     done
