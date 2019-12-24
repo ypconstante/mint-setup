95c0478c0c851c39f0aa85ce82da9c7c091102aa
Yuri Pereira <ypconstante@gmail.com>
2019-12-24 14:59:11 -0300

    Remove docker

--- shell/dev_containers.sh
+++ shell/dev_containers.sh
@@ -2,17 +2,6 @@
 
 source "$(dirname "$0")/_base.sh"
 
-my_step_begin "install docker"
-my_apt_add_key https://download.docker.com/linux/ubuntu/gpg
-my_apt_add_repository docker "deb [arch=amd64] https://download.docker.com/linux/ubuntu $UBUNTU_CODENAME stable"
-my_apt_install docker-ce docker-ce-cli containerd.io
-my_step_end
-
-my_step_begin "add user to docker group"
-sudo groupadd docker
-sudo usermod -aG docker "$USER"
-my_step_end
-
 my_step_begin "install podman"
 my_apt_add_ppa_repository ppa:projectatomic/ppa
 my_apt_install podman --install-recommends
