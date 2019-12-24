a0228e39bc0a1fb167ba2386d4265934273afe1b
Yuri Pereira <ypconstante@gmail.com>
2019-12-24 10:40:59 -0300

    Update virtualbox

--- shell/utility_virtualbox.sh
+++ shell/utility_virtualbox.sh
@@ -5,5 +5,5 @@ source "$(dirname "$0")/_base.sh"
 my_step_begin "install virtualbox"
 my_apt_add_key https://www.virtualbox.org/download/oracle_vbox_2016.asc
 my_apt_add_repository virtualbox "deb https://download.virtualbox.org/virtualbox/debian $UBUNTU_CODENAME contrib"
-my_apt_install virtualbox-6.0
+my_apt_install virtualbox-6.1
 my_step_end
