51ca9133ad912b343f763c4e1cce087a59dc6d16
Yuri Pereira <ypconstante@gmail.com>
2020-01-30 04:29:03 -0300

    Add temporary chrome profile

--- shell/browser_chrome.sh
+++ shell/browser_chrome.sh
@@ -11,3 +11,10 @@ menu_file="$XDG_DATA_HOME/applications/chromium-incognito.desktop"
 my_link_file "$ASSETS_DIR/browser_chrome--incognito-profile.desktop" "$menu_file"
 chmod +x "$menu_file"
 my_step_end
+
+
+my_step_begin "enable temporary chrome"
+menu_file="$XDG_DATA_HOME/applications/chromium-temporary.desktop"
+my_link_file "$ASSETS_DIR/browser_chrome--temp-profile.desktop" "$menu_file"
+chmod +x "$menu_file"
+my_step_end
