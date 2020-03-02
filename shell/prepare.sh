95d7ecef5c55f3d09f4420412ad5f1c6f1c4d992
Yuri Pereira Constante <ypconstante@gmail.com>
2020-03-01 23:19:48 -0300

    Add back default asian fonts

--- shell/prepare.sh
+++ shell/prepare.sh
@@ -18,18 +18,6 @@ my_apt_uninstall xserver-xorg-video-intel
 sudo rm -rf /usr/lib/mono
 my_step_end
 
-
-my_step_begin "remove asian fonts"
-my_apt_uninstall \
-    fonts-kacst* fonts-khmeros* fonts-lklug-sinhala fonts-guru-extra \
-    fonts-nanum* fonts-noto-cjk fonts-takao* fonts-tibetan-machine fonts-lao \
-    fonts-sil-padauk fonts-sil-abyssinica fonts-tlwg-* fonts-lohit-* \
-    fonts-beng-extra fonts-gargi fonts-gubbi fonts-gujr-extra fonts-kalapi \
-    fonts-lohit-* fonts-samyak* fonts-navilu fonts-nakula fonts-orya-extra \
-    fonts-pagul fonts-sarai fonts-telu* fonts-wqy* fonts-smc* fonts-deva-extra
-sudo dpkg-reconfigure fontconfig
-my_step_end
-
 my_step_begin "update packages"
 sudo apt clean -y -qq
 sudo apt update -y -qq
