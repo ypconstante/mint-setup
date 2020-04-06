d1b4fcc09c30f89d336ca669418964da8963e707
Yuri Pereira Constante <ypconstante@gmail.com>
2020-04-05 22:07:06 -0300

    Separate common tools install run

--- shell/dev_common-tools.sh
+++ shell/dev_common-tools.sh
@@ -3,5 +3,8 @@
 source "$(dirname "$0")/_base.sh"
 
 my_step_begin "install common dev tools"
-my_apt_install build-essential g++ gcc make
+my_apt_install build-essential
+my_apt_install g++
+my_apt_install gcc
+my_apt_install make
 my_step_end
