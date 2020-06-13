02e600a0b9ca82adf3c272e9f6d89d41baba092f
Yuri Pereira Constante <ypconstante@gmail.com>
2020-06-13 14:04:49 -0300

    Add docker compose

--- shell/dev_containers.sh
+++ shell/dev_containers.sh
@@ -15,6 +15,18 @@ my_step_begin "config docker"
 my_link_file "$ASSETS_DIR/dev_containers--docker-daemon.json" /etc/docker/daemon.json
 my_step_end
 
+my_step_begin "install docker compose"
+docker_compose_file=$HOME/.local/bin/docker-compose
+docker_compose_url=$( \
+    curl -sS https://api.github.com/repos/docker/compose/releases/latest \
+    | jq '[ .assets[].browser_download_url ]' \
+    | jq '[ .[] | select(endswith("Linux-x86_64")) ]' \
+    | jq -r 'first' \
+)
+curl -L "$docker_compose_url" -o "$docker_compose_file"
+chmod +x "$docker_compose_file"
+my_step_end
+
 my_step_begin "install podman"
 my_apt_add_key https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_18.04/Release.key
 my_apt_add_repository libcontainers "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_18.04/ /"
