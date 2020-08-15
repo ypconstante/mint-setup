cf5272c85451522cf73a5945d542e34e068b3502
Yuri Pereira Constante <ypconstante@gmail.com>
2020-08-15 13:22:11 -0300

    Memory configuration to run Elasticsearch

---
+++ shell/memory.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source "$(dirname "$0")/_base.sh"
+
+my_step_begin "memory kernel parameters"
+sudo cp "$ASSETS_DIR/memory--kernel-parameters.conf" /etc/sysctl.d/98-memory.conf
+sudo chmod 644 /etc/sysctl.d/98-file-system.conf
+my_step_end
