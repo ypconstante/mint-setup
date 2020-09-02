fb8363c643c731001f7cf009cc44127ad3ef4321
Yuri Pereira Constante <ypconstante@gmail.com>
2020-09-01 22:40:46 -0300

    Change zsh theme to starship

--- shell/terminal_zsh.sh
+++ shell/terminal_zsh.sh
@@ -6,6 +6,23 @@ my_step_begin "install zsh"
 my_apt_install zsh
 my_step_end
 
+my_step_begin "install starship"
+starship_compressed_file=/tmp/starship.tar.gz
+starship_file=$HOME/.local/bin/starship
+starship_url=$( \
+    curl -sS https://api.github.com/repos/starship/starship/releases/latest \
+    | jq '[ .assets[].browser_download_url ]' \
+    | jq '[ .[] | select(endswith("linux-gnu.tar.gz")) ]' \
+    | jq -r 'first' \
+)
+
+curl -L "$starship_url" -o "$starship_compressed_file"
+tar -xzf "$starship_compressed_file"  -C "$HOME/.local/bin" starship
+chmod +x "$starship_file"
+my_link_file "$ASSETS_DIR/terminal_zsh--starship.toml" "$XDG_CONFIG_HOME/starship.toml"
+rm "$starship_compressed_file"
+my_step_end
+
 my_step_begin "install zgen"
 my_git_clone https://github.com/tarjoilija/zgen.git "$ZDOTDIR/zgen"
 my_step_end
