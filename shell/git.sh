f1cfe82cf8a99df9baeb22fa0bbd922d61b2fc59
Yuri Pereira Constante <ypconstante@gmail.com>
2020-09-20 17:53:43 -0300

    Add delta as git pager

--- shell/git.sh
+++ shell/git.sh
@@ -39,6 +39,23 @@ cat "$ASSETS_DIR/git--ignore" >> "$git_ignore_file"
 git config --global core.excludesfile "$git_ignore_file"
 my_step_end
 
+
+my_step_begin "install delta"
+delta_compressed_file=/tmp/delta.tar.gz
+delta_file=$HOME/.local/bin/delta
+delta_url=$( \
+    curl -sS https://api.github.com/repos/dandavison/delta/releases/latest \
+    | jq '[ .assets[].browser_download_url ]' \
+    | jq '[ .[] | select(endswith("x86_64-unknown-linux-gnu.tar.gz")) ]' \
+    | jq -r 'first' \
+)
+
+curl -L "$delta_url" -o "$delta_compressed_file"
+tar -xzf "$delta_compressed_file" -C "$HOME/.local/bin" --wildcards "*/delta" --strip-components=1
+chmod +x "$delta_file"
+rm "$delta_compressed_file"
+my_step_end
+
 my_step_begin "config git"
 git config --global include.path "$ASSETS_DIR/git--config"
 git config --global commit.template "$ASSETS_DIR/git--commit-template"
