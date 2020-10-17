b3871372385185ac1b25e15d6b1ba4c4d55ba181
Yuri Pereira Constante <ypconstante@gmail.com>
2020-10-17 19:23:54 -0300

    Change PulseAudio config only for user

--- shell/sound.sh
+++ shell/sound.sh
@@ -11,9 +11,6 @@ dconf write /org/cinnamon/sounds/plug-enabled false
 dconf write /org/cinnamon/sounds/switch-enabled false
 my_step_end
 
-my_step_begin "setup pulse audio"
-# avoid hissing sound when not playing audio
-sudo sed -Ei "s/^load-module module-suspend-on-idle$/#load-module module-suspend-on-idle/" /etc/pulse/default.pa
-# avoid playing music after skype call
-sudo sed -Ei "s/^load-module module-role-cork$/#load-module module-role-cork/" /etc/pulse/default.pa
+my_step_begin "setup pulseaudio"
+my_link_file "$ASSETS_DIR/sound--pulseaudio.po" "$XDG_CONFIG_HOME/pulse/default.po"
 my_step_end
