4fe430bf480242e0d5de72308f1d7b6d8c4d59e2
Yuri Pereira Constante <ypconstante@gmail.com>
2020-11-21 09:43:12 -0300

    Remove podman

--- shell/dev_containers.sh
+++ shell/dev_containers.sh
@@ -26,15 +26,3 @@ docker_compose_url=$( \
 curl -L "$docker_compose_url" -o "$docker_compose_file"
 chmod +x "$docker_compose_file"
 my_step_end
-
-my_step_begin "install podman"
-my_apt_add_key https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_18.04/Release.key
-my_apt_add_repository libcontainers "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_18.04/ /"
-my_apt_install podman --install-recommends
-
-sudo curl -sS https://raw.githubusercontent.com/projectatomic/registries/master/registries.fedora -o /etc/containers/registries.conf
-sudo curl -sS https://raw.githubusercontent.com/containers/skopeo/master/default-policy.json -o /etc/containers/policy.json
-
-# fix "not enough IDs available"
-podman system migrate
-my_step_end
