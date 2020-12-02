61c969492b40a5d2d44347c68f8a9f91f92f51fb
Yuri Pereira Constante <ypconstante@gmail.com>
2020-12-02 06:28:00 -0300

    Fix screenshot directory

--- shell/utility_gnome-screenshot.sh
+++ shell/utility_gnome-screenshot.sh
@@ -3,6 +3,6 @@
 source "$(dirname "$0")/_base.sh"
 
 my_step_begin "config gnome screenshot"
-dconf write /org/gnome/gnome-screenshot/auto-save-directory "'file:///$HOME/downloads/'"
-dconf write /org/gnome/gnome-screenshot/last-save-directory "'file:///$HOME/downloads/'"
+dconf write /org/gnome/gnome-screenshot/auto-save-directory "'file://$HOME/downloads/'"
+dconf write /org/gnome/gnome-screenshot/last-save-directory "'file://$HOME/downloads/'"
 my_step_end
