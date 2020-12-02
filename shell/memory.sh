4a17dd719d729ed96840b1d1deac8385da211d15
Yuri Pereira Constante <ypconstante@gmail.com>
2020-12-01 22:34:38 -0300

    Fix memory config permission

--- shell/memory.sh
+++ shell/memory.sh
@@ -4,5 +4,5 @@ source "$(dirname "$0")/_base.sh"
 
 my_step_begin "memory kernel parameters"
 sudo cp "$ASSETS_DIR/memory--kernel-parameters.conf" /etc/sysctl.d/98-memory.conf
-sudo chmod 644 /etc/sysctl.d/98-file-system.conf
+sudo chmod 644 /etc/sysctl.d/98-memory.conf
 my_step_end
