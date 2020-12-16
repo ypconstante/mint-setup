7e48db34704341c968dbb4d3528d091566d26296
Yuri Pereira Constante <ypconstante@gmail.com>
2020-12-16 18:38:10 -0300

    Add tig xdg fix

--- shell/xdg-fixes.sh
+++ shell/xdg-fixes.sh
@@ -2,6 +2,10 @@
 
 source "$(dirname "$0")/_base.sh"
 
+my_step_begin "xdg tig"
+mkdir "$XDG_DATA_HOME/tig"
+my_step_end
+
 my_step_begin "xdg wget"
 my_append_to_file_if_not_contains "$XDG_CONFIG_HOME/wgetrc" "hsts-file = ${XDG_CACHE_HOME}/wget-hsts"
 rm -f "$HOME/.wget-hsts"
