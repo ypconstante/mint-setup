d02b66e53d6934918bae49c023c407c6ab8ffad2
Yuri Pereira Constante <ypconstante@gmail.com>
2021-01-03 19:49:18 -0300

    Remove path information from grub title

--- shell/grub.sh
+++ shell/grub.sh
@@ -6,7 +6,7 @@ my_step_begin "install grub theme"
 theme_config=/boot/grub/themes/linuxmint/theme.txt
 
 efi_id=$(efibootmgr -v | grep BootCurrent | awk '{print $2}')
-efi_label=$(efibootmgr -v | grep "Boot${efi_id}" | sed 's/[^ ]* *//')
+efi_label=$(efibootmgr | grep "Boot${efi_id}" | sed 's/[^ ]* *//')
 
 my_apt_install grub2-theme-mint --reinstall -o Dpkg::Options::="--force-confmiss"
 my_append_to_file_if_not_contains $theme_config "title-text: \"${efi_label}\""
