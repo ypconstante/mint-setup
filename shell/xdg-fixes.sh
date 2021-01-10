969857dd3a6f4dee351ea79871810a607066e5fe
Yuri Pereira Constante <ypconstante@gmail.com>
2021-01-10 10:44:02 -0300

    Configure warpinator to not save on home

--- shell/xdg-fixes.sh
+++ shell/xdg-fixes.sh
@@ -10,3 +10,7 @@ my_step_begin "xdg wget"
 my_append_to_file_if_not_contains "$XDG_CONFIG_HOME/wgetrc" "hsts-file = ${XDG_CACHE_HOME}/wget-hsts"
 rm -f "$HOME/.wget-hsts"
 my_step_end
+
+my_step_begin "xdg warpinator"
+dconf write /org/x/warpinator/preferences/receiving-folder "'file:///home/ypconstante/downloads/warpinator'"
+my_step_end
