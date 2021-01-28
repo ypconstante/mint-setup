b2f9f9fa4958a1e3115e130ea6b38f53b66e2f9d
Yuri Pereira Constante <ypconstante@gmail.com>
2021-01-27 22:55:31 -0300

    Configure DNS

--- shell/network.sh
+++ shell/network.sh
@@ -10,3 +10,7 @@ my_step_end
 my_step_begin "disable wifi power management"
 sudo sed -Ei 's/wifi.powersave = 3/wifi.powersave = 2/' /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf
 my_step_end
+
+my_step_begin "configure systemd-resolved"
+sudo cp "$ASSETS_DIR/network--systemd-resolved.conf" /etc/systemd/resolved.conf
+my_step_end
